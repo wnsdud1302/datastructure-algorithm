ab00e734b08c3a879e15294ca38c8ec495b570d1
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-09 23:50:48 +0900

    Rename puzzleGame.cpp to 학교/puzzleGame.cpp

--- 학교/puzzleGame.cpp
+++ 학교/puzzleGame.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <conio.h> //getch()�� kbhit() �Լ� ���
-#include <ctime> //clock() �Լ� ���
+#include <conio.h> //getch()와 kbhit() 함수 사용
+#include <ctime> //clock() 함수 사용
 
 using namespace std;
 
-#define ESC 27 //���� ����
-#define LEFT 75 //���� ȭ��ǥ Ű ASCII��
-#define RIGHT 77 // ������ ȭ��ǥ Ű ASCII��
-#define DOWN 80 //�Ʒ� ȭ��ǥ Ű ASCII��
-#define UP 72 //���� ȭ��ǥ Ű ASCII��
+#define ESC 27 //게임 종료
+#define LEFT 75 //왼쪽 화살표 키 ASCII값
+#define RIGHT 77 // 오른쪽 화살표 키 ASCII값
+#define DOWN 80 //아래 화살표 키 ASCII값
+#define UP 72 //위쪽 화살표 키 ASCII값
 
-#define MDIM 4 //4x4 ������� ũ��
-#define PDIM 7 //7x7 ������ ũ��
+#define MDIM 4 //4x4 퍼즐맵의 크기
+#define PDIM 7 //7x7 퍼즐의 크기
 
 
-//���� Map �׸��� ���� ��ġ: �ʿ��ϴٸ� ��� ��ġ�� ������ �� ���� 
+//게임 Map 그리기 시작 위치: 필요하다면 출력 위치를 조정할 수 있음 
 #define MAP_STARTX 20 
 #define MAP_STARTY 2
 
-//�̵� ȸ�� �� �ҿ�ð� ��� ��ġ: �ʿ��ϴٸ� ��� ��ġ�� ������ �� ���� 
+//이동 회수 및 소요시간 출력 위치: 필요하다면 출력 위치를 조정할 수 있음 
 #define SCORE_STARTX 20
 #define SCORE_STARTY 30
 
@@ -144,25 +144,25 @@ class puzzleGame
 {
 public:
 	    
-		puzzleGame(void); //�ʱ�ȭ �Լ�; �ʿ�ÿ� �Ű������� �߰���
-		void gameDraw(void); // ���� �ʰ� �ݺ�ȸ��, �ð� ����Լ�; MAP_STARTX, MAP_STARTY ����� �����ؼ� �׻� ���� ��ġ(gotoXY�Լ� ��뿡 ����ʿ�ÿ� �Ű������� �߰���
+		puzzleGame(void); //초기화 함수; 필요시에 매개변수를 추가함
+		void gameDraw(void); // 게임 맵과 반복회수, 시간 출력함수; MAP_STARTX, MAP_STARTY 상수를 참조해서 항상 고정 위치(gotoXY함수 사용에 출력필요시에 매개변수를 추가함
 		void puzzleDraw(int (*puzzle)[PDIM], int x, int y);//
-		void puzzleMove(int moveKey); //���� �̵� �Լ�; �ʿ�ÿ� �Ű����� �߰���
-		void shuffle(int num);//���� �� �ʱ�ȭ; 50ȸ ������ �̵�, �ʿ�ÿ� ���� �߰� �� �� ����
-		void getCommand(void);//��� ������ �� ��ġ�� �ְų� ESCŰ�� ���������� ���� 0�� ��ȯ
-                             // ����Ű�� ������ ���� �ش� Ű���� ��ȯ(LEFT, RIGHT, DOWN, UP)
-                             //�ʿ�ÿ� �Ű����� ���ڸ� �߰��� �� ����
+		void puzzleMove(int moveKey); //퍼즐 이동 함수; 필요시에 매개변수 추가함
+		void shuffle(int num);//퍼즐 맵 초기화; 50회 무작위 이동, 필요시에 인자 추가 할 수 있음
+		void getCommand(void);//모든 퍼즐이 제 위치에 있거나 ESC키가 눌러졌으면 정수 0을 반환
+                             // 방향키를 눌렀을 때는 해당 키값을 반환(LEFT, RIGHT, DOWN, UP)
+                             //필요시에 매개변수 인자를 추가할 수 있음
 		int getAction(void)	{ return action;};
 private:
 	   int map[MDIM][MDIM];
        int action;
-	   int x,y; //��ĭ�� ��ġ
-	   int moveNum; //�̵�ȸ��
-	   clock_t sTime; //���� �ð�
-	   clock_t cTime; //���� �ð�
+	   int x,y; //빈칸의 위치
+	   int moveNum; //이동회수
+	   clock_t sTime; //시작 시간
+	   clock_t cTime; //시작 시간
 };
 
-void gotoXY(int x, int y); //�ܼ� ȭ�鿡�� Ŀ���� Ư�� ��ġ�� �̵�
+void gotoXY(int x, int y); //콘솔 화면에서 커서를 특정 위치로 이동
 int getDirectKey();
 
 int main(void)
@@ -185,7 +185,7 @@ int main(void)
 puzzleGame::puzzleGame()
 {
 
-	//���� �ʱ�ȭ �ڵ� �ۼ�
+	//게임 초기화 코드 작성
 	
 }
 
@@ -200,9 +200,9 @@ void puzzleGame::puzzleDraw(int (*puzzle)[PDIM], int x, int y)
 			if(puzzle[i][j] == 0)
 				cout << "  ";
 			else if(puzzle[i][j] == 1)
-				cout << "��";
+				cout << "□";
 			else
-				cout << "��";
+				cout << "■";
 		
 		}
 	}
@@ -218,17 +218,17 @@ void puzzleGame::gameDraw(void)
 		for(int j=0; j< MDIM; j++)
 		{
 
-			//���⼭ puzzleDraw �Լ� ȣ����
+			//여기서 puzzleDraw 함수 호출함
 
 		}
 	}
 
 	cout << endl;
 	gotoXY(  SCORE_STARTX, SCORE_STARTY);
-	cout << "  �̵� ȸ��: " << moveNum << endl;
+	cout << "  이동 회수: " << moveNum << endl;
 	gotoXY(  SCORE_STARTX, SCORE_STARTY+1);
 	cTime = clock();
-	cout << "  �ҿ� �ð�: " << (cTime-sTime)/1000.0 << "��" << endl;
+	cout << "  소요 시간: " << (cTime-sTime)/1000.0 << "초" << endl;
 }
 
 
@@ -247,7 +247,7 @@ void puzzleGame::getCommand(void)
 	~
 }
 
-//�ܼ� ȭ�鿡�� Ŀ���� Ư�� ��ġ�� �̵�
+//콘솔 화면에서 커서를 특정 위치로 이동
 void gotoXY(int x, int y) 
 {
  COORD Pos = {x, y};
@@ -258,10 +258,10 @@ void gotoXY(int x, int y)
 int getDirectKey()
 {
 	char key=Non;
-    if(_kbhit()!= 0) //Ű���带 �������� Ȯ����
+    if(_kbhit()!= 0) //키보드를 눌렀는지 확인함
 	{
-	  //Ư�� Ű�� ������ �� ���ۿ� 2Byte�� �߻���, ù��° ���� 224���� �߻��ϰ� �ι�° ���� Ư��Ű�� ���� �ٸ�
-	  //Ư�� Ű�� Ȯ���ϱ� ���� 2���� getch()�Լ��� ȣ���ؾ� ��
+	  //특수 키를 눌렀을 때 버퍼에 2Byte가 발생함, 첫번째 값은 224값을 발생하고 두번째 값은 특수키에 따라 다름
+	  //특수 키를 확인하기 위해 2번의 getch()함수를 호출해야 함
 		key = getch();
 		if(key==224) key=_getch();
 	}
