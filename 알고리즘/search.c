4f77beb9a6f92e73ebc67d57ac998983df6c4193
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-09 23:06:34 +0900

    Update search.c

--- 알고리즘/search.c
+++ 알고리즘/search.c
@@ -20,21 +20,91 @@ int search(const int ar[], int key, int len){
     }
 }
 
+int bin_search1(const int ar[], int key, int len){
+    int pl = 0;
+    int pr = len - 1;
+    int pc;
+    do{
+        pc = (pl+pr) / 2;
+        if (ar[pc] == key)
+            return pc;
+        else if (ar[pc] < key)
+            pl = pc + 1;
+        else if (ar[pc] > key)
+            pr = pc - 1;
+    } while(pl <= pr);
+    return -1;
+}
+
+int bin_search2(const int ar[], int key, int len){
+    int pl = 0;
+    int pc = 0;
+    int pr = len - 1;
+    int i = 1;
+    do{
+        pc = (pl + pr) / 2;
+        if(ar[pc] == key){
+            do{
+                if(ar[pc-i] != key)
+                    return pc - i + 1;
+                i++;
+            } while( pc- i >= pl);
+        }
+        else if (ar[pc] < key)
+            pl = pc + 1;
+        else if ( ar[pc] > key)
+            pr = pc - 1;
+    } while(pl <= pr);
+    return -1;
+}
+
 int main(){
     int *ar;
     int max;
-    int s;
     int num = 10;
     ar = calloc(num, sizeof(int));
-    for(int i = 0; i < 10; i++){
-        ar[i]  = rand() % 10;
+    for(int i = 0; i < num; i++){
+        printf("ar[%d]: ", i);
+        scanf("%d", &ar[i]);
     }
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < num; i++)
         printf("%d ", ar[i]);
+    while (1){
+        int menu, x, print;
+
+        
+        puts("\n");
+        printf("(1: search, 2: bin search1, 3: bin search2, 0: 종료)\n 선택하세요 : ");
+        scanf("%d", &menu);
+        if(menu == 0){
+            free(ar);
+            break;
+        }
+        switch (menu)
+        {
+        case 1:
+            printf("데이터 :");
+            scanf("%d", &x);
+            print = search(ar, x, num);
+            printf("%d \n", print);
+            break;
+        
+        case 2:
+         printf("데이터 :");
+            scanf("%d", &x);
+            print = bin_search1(ar, x, num);
+            printf("%d\n", print);
+            break;
+
+        case 3:
+            printf("데이터 :"); 
+            scanf("%d", &x);
+            print = bin_search2(ar, x, num);
+            printf("%d\n", print);
+            break;
+        }
     }
-    printf("\n");
-    max = FindMax(ar, 10);
-    printf("%d\n", max);
-    free(ar);
+
     return 0;
 }
+
