04eca36b3f215a9fd8a5e6c24e40a9b39d1dcc34
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-10 16:23:47 +0900

    Create Stack.h

---
+++ c++/Stack.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+
+class Stack{
+    public:
+    Stack();
+    Stack(int n);
+    int Push(int x);
+    int Pop(int *x);
+    int Peek(int *x);
+    void clear();
+    int capacity();
+    int size();
+    bool isEmpty();
+    bool isFull();
+    int Search(int x);
+    void Print();
+    void Terminate();
+    private:
+    int max;
+    int ptr;
+    int *stk;
+};
+#endif
