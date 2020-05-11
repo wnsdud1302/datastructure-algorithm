0e198300602852eb0f90b15da66cb6ea1845a637
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-11 23:06:31 +0900

    Create Queue.h

---
+++ c++/Queue/Queue.h
@@ -0,0 +1,28 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+class queue
+{
+private:
+    int front;
+    int rear;
+    int que;
+    int num;
+    int max;
+public:
+    queue();
+    queue(int max);
+    void Enque(int x);
+    void Deque(int *x);
+    void Peek(int *x);
+    void Clear();
+    int Capacity();
+    int Size();
+    bool IsEmpty();
+    bool IsFull();
+    void Search(int *x);
+    void Print();
+    ~queue();
+};
+
+#endif
