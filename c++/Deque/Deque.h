2d9428d5efca11565bd85a716c703089dbe7e830
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-26 23:18:17 +0900

    Add files via upload

--- c++/Deque/Deque.h
+++ c++/Deque/Deque.h
@@ -1,1 +1,125 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+#include<iostream>
+#include<string>
+#include"Double_linked_list.h"
+using namespace std;
 
+template<typename T>
+class Deque{
+public:
+    Deque();
+    int size() const;
+    bool IsEmpty() const;
+    const T& Front() const;
+    const T& Back() const;
+    const T& Data(int k) const;
+    const T& operator[](int k) const;
+    const T& operator[](int k);
+    void InsertFront(const T& data);
+    void InsertBack(const T& data);
+    void RemoveFront();
+    void RemoveBack();
+    void Clear();
+    void Print();
+    void PrintReverse();
+private:
+    Double_linked_list<T> l;
+    int n;
+};
+
+template<typename T>
+Deque<T>::Deque(){ n = 0 ;}
+
+template<typename T>
+int Deque<T>::size() const{
+    return n;
+}
+
+template<typename T>
+bool Deque<T>::IsEmpty() const{
+    return l.IsEmpty();
+}
+
+template<typename T>
+const T& Deque<T>::Front() const{
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    return l.Front();
+}
+
+template<typename T>
+const T& Deque<T>::Back() const{
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    return l.Back();
+}
+
+template<typename T>
+const T& Deque<T>::Data(int k) const{
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    return l.Data(k);
+}
+
+template<typename T>
+const T& Deque<T>::operator[](int k) const{
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    return Data(k);
+}
+
+template<typename T>
+void Deque<T>::InsertFront(const T& data){
+    l.InsertFront(data);
+    n++;
+}
+
+template<typename T>
+void Deque<T>::InsertBack(const T& data){
+    l.InsertBack(data);
+    n++;
+}
+
+template<typename T>
+void Deque<T>::RemoveFront(){
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    l.RemoveFront();
+    n--;
+}
+
+template<typename T>
+void Deque<T>::RemoveBack(){
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    l.RemoveBack();
+    n--;
+}
+
+template<typename T>
+void Deque<T>::Clear(){
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    while(!IsEmpty()){
+        RemoveBack();
+        n--;
+    }
+}
+
+template<typename T>
+void Deque<T>::Print(){
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    l.Print();
+}
+
+template<typename T>
+void Deque<T>::PrintReverse(){
+    if(IsEmpty())
+        cout << "데크가 비어있습니다." << endl;
+    l.PrintReverse();
+}
+
+
+#endif
---
+++ c++/Deque/Double_linked_list.h
@@ -0,0 +1,223 @@
+#ifndef DOUBLE_LINKED_LIST_H
+#define DOUBLE_LINKED_LIST_H
+#include<iostream>
+#include<string>
+using namespace std;
+
+template <typename T> class Double_linked_list;
+
+template <typename T>
+class Node{
+public:
+    void SetNode(const T& x,Node<T> *nxt,Node<T> *prv);
+private:    
+    T data;
+    Node<T> *next;
+    Node<T> *prev;
+    friend class Double_linked_list<T>;
+
+};
+template <typename T>
+class Double_linked_list{
+public:
+    Double_linked_list();
+    ~Double_linked_list();
+    bool IsEmpty() const;
+    const T& Front() const;
+    const T& Back() const;
+    const T& Data(int k) const;
+    void InsertFront(const T& data);
+    void InsertBack(const T& data);
+    void RemoveFront();
+    void RemoveBack();
+    Node<T> *Search(const T& data);
+    bool Next();
+    bool Prev();
+    void Printcurrent();
+    void Print();
+    void PrintReverse();
+    void Clear();
+    void addSorted(const T& data);
+private:
+    Node<T> *head;
+    Node<T> *tail;
+    Node<T> *crnt;
+protected:
+    void Insert(Node<T> *n,const T& data);
+    void Remove(Node<T> *n);
+
+};
+
+template<typename T>
+void Node<T>::SetNode(const T& x, Node<T> *nxt, Node<T> *prv){
+    data = x;
+    next = nxt;
+    prev = prv;
+}
+
+template<typename T>
+Double_linked_list<T>::Double_linked_list(){
+    head = new Node<T>;
+    tail = new Node<T>;
+    head->next = tail;
+    tail->prev = head;
+    crnt = NULL;
+}
+
+template<typename T>
+Double_linked_list<T>::~Double_linked_list(){
+    while(!IsEmpty())
+        RemoveFront();
+    delete head;
+    delete tail;
+}
+
+template<typename T>
+bool Double_linked_list<T>::IsEmpty() const{
+    return (head->next == tail || crnt == NULL);
+}
+
+template<typename T>
+const T& Double_linked_list<T>::Front() const{
+    return head->next->data;
+}
+
+template<typename T>
+const T& Double_linked_list<T>::Back() const{
+    return tail->prev->data;
+}
+
+template<typename T>
+const T& Double_linked_list<T>::Data(int k) const{
+    Node<T> *start = head;
+    for(int i = 0; i < k; i++){
+        if(start == tail)
+            break;
+        start = start->next;
+    }
+    return start->data;
+}
+
+template<typename T>
+void Double_linked_list<T>::Insert(Node<T> *n,const T& data){
+    Node<T> *new_node = new Node<T>;
+    Node<T> *nxt = n->next;
+    n->next = n->next->prev = new_node;
+    new_node->SetNode(data, nxt, n);
+    crnt = new_node;
+}
+
+template<typename T>
+void Double_linked_list<T>::Remove(Node<T> *n){
+    n->prev->next = n->next;
+    n->next->prev = n->prev;
+    delete n;
+}
+
+template<typename T>
+void Double_linked_list<T>::InsertFront(const T& data){
+    Insert(head, data);
+    crnt = head->next;
+}
+
+template<typename T>
+void Double_linked_list<T>::InsertBack(const T& data){
+    Insert(tail->prev, data);
+    crnt = tail->prev;
+}
+
+template<typename T>
+void Double_linked_list<T>::RemoveFront(){
+    Remove(head->next);
+}
+
+template<typename T>
+void Double_linked_list<T>::RemoveBack(){
+    Remove(tail->prev);
+}
+
+template<typename T>
+Node<T> *Double_linked_list<T>::Search(const T& data){
+    Node<T> *start = head;
+    while(start->next != tail){
+        if(start->data == data){
+            crnt = start;
+            return start;
+        }
+        start = start->next;
+    }
+    return NULL;
+}
+
+template<typename T>
+bool Double_linked_list<T>::Next(){
+    if(IsEmpty() || crnt->next == NULL)
+        return false;
+    crnt = crnt->next;
+    return true;
+}
+
+template<typename T>
+bool Double_linked_list<T>::Prev(){
+    if(IsEmpty() || crnt->prev == NULL)
+        return false;
+    crnt = crnt->prev;
+    return true;
+}
+
+template<typename T>
+void Double_linked_list<T>::Printcurrent(){
+    if(crnt == NULL)
+        cout << "선택한 노드가 없습니다." << endl;
+    cout << crnt->data << "입니다." << endl;
+}
+
+template<typename T>
+void Double_linked_list<T>::Print(){
+    Node<T> *start = head->next;
+    while(start != tail){
+        cout << start->data << " <=> ";
+        start = start->next;
+    }
+    putchar('\n');
+}
+
+template<typename T>
+void Double_linked_list<T>::PrintReverse(){
+    Node<T> *start = tail->prev;
+    while(start != head){
+        cout << start->data << " <=> ";
+        start = start->prev;
+    }
+    putchar('\n');
+}
+
+template<typename T>
+void Double_linked_list<T>::Clear(){
+    while(head->next != NULL)
+        RemoveFront();
+    crnt = NULL;
+}
+
+template<typename T>
+void Double_linked_list<T>::addSorted(const T& data){
+    Node<T> *start = head->next;
+    if(head->next == tail){
+        Insert(head, data);
+        crnt = head->next;
+    }
+    else{
+        while(start->next != tail){
+            if(start->data > data && start->next == tail){
+                Insert(start, data);
+                break;
+            }
+            else if(start->data > *data && start->next->data < data){
+                Insert(start, data);
+                break;
+            }
+        start = start->next;
+        }
+    }
+}
+#endif
