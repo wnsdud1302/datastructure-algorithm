3f0625eb88a1f7d4bfafa162b9a4811c6cf37b1a
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-27 23:36:49 +0900

    Add files via upload

---
+++ c++/Array_Vector/Array_Vector.cpp
@@ -0,0 +1,54 @@
+#include"Array_Vector.h"
+
+ArrayVector::ArrayVector() : n(0), capacity(0), d(NULL) {}
+
+int ArrayVector::size() const{
+    return n;
+}
+
+bool ArrayVector::isEmpty() const{
+    return (size() == 0);
+}
+
+Elem& ArrayVector::Front() const{
+    return E[0];
+}
+
+Elem& ArrayVector::Back() const{
+    return E[n];
+}
+
+Elem& ArrayVector::operator[](int i){
+    return E[i];
+}
+
+Elem& ArrayVector::At(int i){
+    if(i < 0 || i >= n)
+        cout << " illegal index in function at()" <<endl;
+    return E[i];
+}
+
+void ArrayVector::Insert(int i, const Elem& e){
+    if(n >= capacity)
+        Reserve(max(1,2*capacity));
+    for(int j = n-1; j>= i; j--)
+        E[j+1] = E[j];
+    E[i] = e;
+    n++;
+}
+
+void ArrayVector::Erase(int i){
+    for(int j = i+1; j < n; j++)
+        E[j-1] = E[j];
+    n--;
+}
+
+void ArrayVector::Reserve(int N){
+    if(capacity >= N) return;
+    Elem *B = new Elem[N];
+    for(int j = 0; j < n; j++)
+        B[j] = E[j];
+    if(E != NULL) delete [] E;
+    E = B;
+    capacity = N;
+}
