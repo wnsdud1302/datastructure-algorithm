a7e9478cc872636c44f71d98492daa0236a84050
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-06-05 22:54:06 +0900

    Add files via upload

--- 알고리즘/sort.c
+++ 알고리즘/sort.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+//버블 정렬
 void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
@@ -8,7 +9,7 @@ void swap(int *a, int *b){
 
 void BubbleSort(int a[], int n){
     for(int i = 0; i < n-1; i++){
-        for(int j = n-1; j > 0; j--){
+        for(int j = n-1; j > i; j--){
             if(a[j-1] > a[j])
                 swap(&a[j-1], &a[j]);
         }
@@ -16,5 +17,60 @@ void BubbleSort(int a[], int n){
 }
 
 void BubbleSort1(int a[], int n){
-    int exg
+    for(int i = 0; i < n; i++){
+        int exg = 0;
+        for(int j = n-1; j > i; j--){
+            if(a[j-1] > a[j])
+                swap(&a[j-1], &a[j]);
+            exg++;
+        }
+        if(exg == 0) break;
+    }
 }
+
+int is_sorted(int a[], int n){
+    for(int i = 0; i < n; i++){
+        int exg = 0;
+        for(int j = n-1; j > i; j--){
+            if(a[j-1] > a[j])
+                exg++;
+        }
+        if(exg == 0) return 1;
+    }
+    return 0;
+}
+
+void BubbleSort2(int a[], int n){
+    int k = 0;
+    for(int i = 0; i < n; i++){
+        int last = n-1;
+        for(int j = n-1; j > i; j--){
+            if(a[j-1] > a[j])
+                swap(&a[j-1], &a[j]);
+            last = j;
+        }
+        k = last;
+    }
+}
+
+void BubbleSort3(int a[], int n){
+    int left = 0;
+    int right = n-1;
+    int last = right;
+    while(left < right){
+        for(int i = left; i < right-1; i++){
+            if(a[i] > a[i+1])
+                swap(&a[i] , &a[i+1]);
+            last = i;
+        }
+        right = last;
+        for(int i = right; i > left; i--){
+            if(a[i-1] > a[i])
+                swap(&a[i-1], &a[i]);
+            last = i;
+        }
+        left = last;
+    }
+}
+
+//단순 선택 정렬
